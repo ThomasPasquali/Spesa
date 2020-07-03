bfac8ad21a8e75fc3d2c1c8d51d283e7f238698b
Thomas <tom.pasqui00@gmail.com>
2020-07-03 21:29:05 +0200

    Varie modifiche

--- public/js/gestioneOggetti.js
+++ public/js/gestioneOggetti.js
@@ -4,7 +4,7 @@ $(document).ready(async function() {
 
     var table = new Tabulator('#table', {
         layout: 'fitColumns',
-        movableRows: true,
+        movableRows: false,
         groupBy: 'SupermercatoID',
         headerSort: false,
         groupStartOpen: false,
@@ -19,13 +19,22 @@ $(document).ready(async function() {
             {title: 'Nome', field: 'Nome', editor: 'input', widthGrow:5},
             {title: 'Note', field: 'Note', editor: 'input', minWidth: '80px'},
             {title: 'Prezzo €', field: 'Prezzo', editor: 'number', editorParams: {min: 0.05, max: 999.99, step: 0.05}, minWidth: '80px'},
+            {title: 'Elimina', field: 'Elimina', cellClick:function(e, cell) {
+                sendRequest('/delete/oggetto', {'ID':cell.getRow().getCell("ID").getValue()});
+                cell.getRow().delete();
+            }},
         ],
+        ajaxResponse:function(url, params, data){
+            for (const row of data) row.Elimina = "🗑️";
+            return data;
+        },
         cellEdited: (cell) => {
             sendRequest('/update/oggetto', cell._cell.row.data);
         },
         ajaxURL: 'get/oggetti',
         ajaxConfig: 'POST',
-        tabEndNewRow: true
+        tabEndNewRow: true,
+        addRowPos: "top"
     });
 
     sendRequest('/get/supermercati', null).then((supermercati) => {
@@ -37,6 +46,7 @@ $(document).ready(async function() {
         e.preventDefault();
         let form = $('#newOggetto');
         sendRequest('/insert/oggetto', form.serialize()).then((res) => {
+            res[0].Elimina = "🗑️";
             table.addData(res[0]);
             form.trigger('reset');
         });
